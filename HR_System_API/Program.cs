ffaf996ec8de69249307eeb90aa5d2d156db40d1
ye <xinyeyang0723@gmail.com>
2025-12-30 22:28:42 +0800

    cors設定

--- HR_System_API/Program.cs
+++ HR_System_API/Program.cs
@@ -12,6 +12,17 @@ namespace HR_System_API
             // 註冊 EF Core 的 DbContext
             builder.Services.AddDbContext<HrContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            //加入 CORS 設定
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngularDev",
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:4200") // 允許 Angular 的網址
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+                    });
+            });
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
@@ -27,7 +38,7 @@ namespace HR_System_API
             }
 
             app.UseHttpsRedirection();
-
+            app.UseCors("AllowAngularDev");
             app.UseAuthorization();
 
 
